c391c80cb450b4b9b7881236b1a772c91336b2cd
gafernan <39523875+gafernan@users.noreply.github.com>
2019-11-15 15:38:49 +0400

    New changes to capstone project

--- capstone.py
+++ capstone.py
@@ -4,7 +4,7 @@ Created on Fri Nov 15 15:28:50 2019
 
 @author: ferna
 """
-
+print ("this course would be used for data science project based on capstone")
 import pandas as pd
 import numpy as np
 print ("Hello Capstone project course!")
