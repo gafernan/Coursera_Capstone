da14e081c572655aea301fcd1d8b9c32a8059a67
gafernan <39523875+gafernan@users.noreply.github.com>
2019-11-15 15:34:40 +0400

    updated project

--- capstone.py
+++ capstone.py
@@ -1,1 +1,10 @@
-import pandas
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 15 15:28:50 2019
+
+@author: ferna
+"""
+
+import pandas as pd
+import numpy as np
+print ("Hello Capstone project course!")
